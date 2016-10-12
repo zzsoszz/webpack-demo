79bbd3a3d3fd0d848edd64a367fc8120530430fd
hyq2015 <18030638805@163.com>
2016-10-12 15:09:44 +0800

    update

--- example.ensure.js
+++ example.ensure.js
@@ -8,5 +8,28 @@ require.ensure(["./module1",'jquery'], function(require) {
 	   	 var module2 = require("bundle!./module2");
 		 console.log("bbb");
     });
-
+    
 });
+
+
+// var a = require("a");
+
+// require.ensure(["b"], function(require) {
+//     // a named chunk
+//     var c = require("c");
+// }, "my own chunk");
+
+// require.ensure(["b"], function(require) {
+//     // another chunk with the same name
+//     var d = require("d");
+// }, "my own chunk");
+
+// require.ensure([], function(require) {
+//     // the same again
+// }, "my own chunk");
+
+// require.ensure(["b"], function(require) {
+//     // chunk without name
+//     var d = require("d");
+// });
+
